c3dfc4e5f2225b23aa3874185695e42ad08f2f66
chrisstophe <christophe.rezk@gmail.com>
2022-06-14 15:49:27 -0400

    Added routes for individual sessions, rendered pages for each, and separated the sessionsRouter into its own file

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
-
 const PORT = process.env.PORT || 3000;
 
 // Create an instance of express
 const app = express();
-const sessionsRouter = express.Router();
+const sessionsRouter = require("./src/routers/sessionsRouter");
 
 // Middleware
 app.use(morgan("tiny"));
@@ -17,20 +16,6 @@ app.set("views", `${__dirname}/src/views`);
 app.set("view engine", "ejs");
 
 // Implementing sessions
-sessionsRouter.route("/").get((req, res) => {
-  res.render("sessions", {
-    sessions: [
-      { title: "Session 1", description: "this is session 1" },
-      { title: "Session 2", description: "this is session 2" },
-      { title: "Session 3", description: "this is session 3" },
-      { title: "Session 4", description: "this is session 4" },
-    ],
-  });
-});
-
-sessionsRouter.route("/1").get((req, res) => {
-  res.send("hello single sessions");
-});
 
 app.use("/sessions", sessionsRouter);
 
